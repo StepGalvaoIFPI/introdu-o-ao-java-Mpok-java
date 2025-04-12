3d4bc5c56474931f37843bf6e76653199b3f641b
MárioCoêlhoIFPI <mariocoelho0510@gmail.com>
2025-04-11 23:57:36 -0300

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -1,6 +1,16 @@
-//teste
+import java.util.Scanner;
+
 public class Q1{
-  public class void main(){
-    System.out.printf("Welcome");
-  }
+    
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Digite a base:");
+        float base = Integer.valueOf(entrada.nextLine());
+        System.out.print("Digite a altura:");
+        float altura = Integer.valueOf(entrada.nextLine());
+        
+        float area = (base * altura)/2;
+        System.out.println("Area = "+area);
+    }
 }
